70bcc985a280c049e9b8f46a8a8d76c2241cf25a
FrederikBRoth <frederikbroth@gmail.com>
2023-03-22 11:48:36 +0100

    Removed unnecesary files

--- tut1/target/debug/deps/tut1-2f21dda3c3e847e8.d
+++
@@ -1,5 +0,0 @@
-/home/fred/Documents/Rust/RustPractice/tut1/target/debug/deps/tut1-2f21dda3c3e847e8.rmeta: src/main.rs
-
-/home/fred/Documents/Rust/RustPractice/tut1/target/debug/deps/tut1-2f21dda3c3e847e8.d: src/main.rs
-
-src/main.rs:
--- tut1/target/debug/deps/tut1-8e696cf1fc0967a9.d
+++
@@ -1,5 +0,0 @@
-/home/fred/Documents/Rust/RustPractice/tut1/target/debug/deps/tut1-8e696cf1fc0967a9.rmeta: src/main.rs
-
-/home/fred/Documents/Rust/RustPractice/tut1/target/debug/deps/tut1-8e696cf1fc0967a9.d: src/main.rs
-
-src/main.rs:
