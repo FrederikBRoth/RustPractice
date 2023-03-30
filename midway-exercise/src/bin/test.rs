b2d7e6470cdc9a39a531e8feb72a910178b8d8d4
FrederikBRoth <frederikbroth@gmail.com>
2023-03-30 17:27:56 +0200

    added tests

--- midway-exercise/src/bin/test.rs
+++ midway-exercise/src/bin/test.rs
@@ -1,20 +1,3 @@
-use clap::Parser;
-
-#[derive(Debug, Parser, Default)]
-#[command(about, version, no_binary_name(true))]
-struct Cli {
-    #[arg(long, short, default_value_t = String::from("Default endpoint"))]
-    /// RPC endpoint of the node that this wallet will connect to
-    endpoint: String,
-
-    #[arg(long, short)]
-    refresh_rate: Option<u32>,
-}
-
 fn main() {
-    let input = vec!["--endpoint", "localhost:8000"];
-
-    let c = Cli::parse_from(input);
-
-    println!("{:?}", c);
+    println!("awdawd")
 }
--- midway-exercise/src/main.rs
+++ midway-exercise/src/main.rs
@@ -1,41 +1,52 @@
-use clap::Parser;
+use clap::{Parser, ValueEnum};
 use std::collections::HashMap;
-use std::io;
 
-#[derive(Parser, Debug, Default)]
-#[command(author, version, about, no_binary_name(true))]
-struct NipponArgs {
-    #[arg(short, long)]
-    word: String,
-    #[arg(short, long)]
-    reverse: Option<String>,
+#[derive(ValueEnum, Clone, Debug)]
+enum Mode {
+    Read,
+    Write,
 }
+
+#[derive(Parser, Debug)]
+#[command(author, version, about)]
+struct Opt {
+    #[clap(value_enum, default_value_t=Mode::Read)]
+    mode: Mode,
+    #[arg(long, default_value = "gaijin")]
+    word: String,
+}
+
 fn main() {
+    let args = Opt::parse();
     let words = HashMap::from([
         ("gaijin", "foreigner"),
         ("baka", "viktor"),
         ("chinchin", "dick"),
     ]);
-
     println!("Yo write in a word to get translated");
-    loop {
-        let mut input = String::new();
-        io::stdin().read_line(&mut input).expect("Shit fucked");
-        if input.trim() == "Stop" {
-            break;
-        }
 
-        let array = input.trim().split(' ');
-        let args = NipponArgs::parse_from(array);
-
-        let word: &str = &args.word;
-        if let Some(w) = words.get(word) {
-            println!("Translated word biiitch: {}", w);
-        } else {
-            println!(
-                "Our very expansive wordlist doesn't contain the word: {}",
-                word
-            );
+    match args.mode {
+        Mode::Read => {
+            println!("Reads the current file")
         }
+        Mode::Write => {
+            let word: &str = &args.word;
+            if let Some(w) = words.get(word) {
+                println!("Translated word biiitch: {}", w);
+            } else {
+                println!(
+                    "Our very expansive wordlist doesn't contain the word: {}",
+                    word
+                );
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn testtest() {
+        println!("This is a test");
     }
 }
